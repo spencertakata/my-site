0ea03be2256aa4757129304b6842ce0df313513b
spencer <spencer.takata@gmail.com>
2019-11-21 20:17:49 -0800

    dynamic footer photo credit

--- public/js/myScripts.js
+++ public/js/myScripts.js
@@ -33,7 +33,39 @@ $(document).ready(function() {
 	});
 });
 
+//Footer photo creditCopyText
+var url = window.location.href
+var home = "https://spencertakata.com/"
+var index = "https://spencertakata.com/index.html"
+var about = "https://spencertakata.com/about.html"
+var services = "https://spencertakata.com/services.html"
 
+if(url == home || url == index){
+  $(function(){
+    $("#photo-cred").html(<a href="https://unsplash.com/@robertbye">Rob Bye</a>);
+  };
+};
+
+else if (url == about){
+  $(function(){
+    $("#photo-cred").html(<a href="https://unsplash.com/@cbarbalis">Chris Barbalis</a>);
+  };
+};
+
+else if (url == services){
+  $(function(){
+    $("#photo-cred").html(<a href="https://unsplash.com/@anete_lusina">Anete Lūsiņa</a>);
+  });
+};
+
+else if (url == "https://spencertakata.com/contact.html"){
+
+}
+else {
+  $(function(){
+    $(".footnote").css("display", "none");
+  };
+};
 
 //Background images
 $("#jumboHome").css("background-image", "url(https://i.imgur.com/6tS1WVR.jpg)");
@@ -43,5 +75,3 @@ $("#jumboServices").css("background-image", "url(https://i.imgur.com/5rCEzKW.jpg
 
 //Hide Blog
 $(".blog").css("display", "none");
-
-$(".scrollTo").click(function(){return $("html, body").animate({scrollTop:$($(this).attr("href")).offset().top},500),!1}),$(document).ready(function(){$(".popup-gallery").magnificPopup({delegate:"a",type:"image",tLoading:"Loading image #%curr%...",mainClass:"mfp-img-mobile",gallery:{enabled:!0,navigateByImgClick:!0,preload:[0,1]},image:{tError:'<a href="%url%">The image #%curr%</a> could not be loaded.',titleSrc:function(e){return e.el.attr("title")+"<small>by Marsel Van Oosten</small>"}}})}),$("#jumboHome").css("background-image","url(https://i.imgur.com/6tS1WVR.jpg)"),$("#jumboAbout").css("background-image","url(https://i.imgur.com/H4rzWkb.jpg)"),$("#jumboGallery").css("background-image","url(https://i.imgur.com/wU2wpBO.jpg)"),$("#jumboServices").css("background-image","url(https://i.imgur.com/5rCEzKW.jpg)"),$(".blog").css("display","none");
