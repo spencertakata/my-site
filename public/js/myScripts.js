60248d91b193b72d917a216ea02b6725e660ae50
spencer <spencer.takata@gmail.com>
2019-11-21 19:24:25 -0800

    implement jquery calls for common modules

--- public/js/myScripts.js
+++ public/js/myScripts.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 $(document).ready(function(){
     $("#nav-placeholder").load("nav.html");
@@ -43,6 +43,5 @@ $("#jumboServices").css("background-image", "url(https://i.imgur.com/5rCEzKW.jpg
 
 //Hide Blog
 $(".blog").css("display", "none");
-=======
+
 $(".scrollTo").click(function(){return $("html, body").animate({scrollTop:$($(this).attr("href")).offset().top},500),!1}),$(document).ready(function(){$(".popup-gallery").magnificPopup({delegate:"a",type:"image",tLoading:"Loading image #%curr%...",mainClass:"mfp-img-mobile",gallery:{enabled:!0,navigateByImgClick:!0,preload:[0,1]},image:{tError:'<a href="%url%">The image #%curr%</a> could not be loaded.',titleSrc:function(e){return e.el.attr("title")+"<small>by Marsel Van Oosten</small>"}}})}),$("#jumboHome").css("background-image","url(https://i.imgur.com/6tS1WVR.jpg)"),$("#jumboAbout").css("background-image","url(https://i.imgur.com/H4rzWkb.jpg)"),$("#jumboGallery").css("background-image","url(https://i.imgur.com/wU2wpBO.jpg)"),$("#jumboServices").css("background-image","url(https://i.imgur.com/5rCEzKW.jpg)"),$(".blog").css("display","none");
->>>>>>> d91fee3c59028c78b7d445c9abf760509b4fc4c1
