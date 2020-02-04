93db2f35c73879edc5eadf0b9b0c9cba947b4653
Spencer Takata <stakata@google.com>
2020-02-04 11:04:38 -0800

    added gulp-purifycss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
+var purify = require('gulp-purifycss');
 
-gulp.task('default', function () {
+gulp.task('nunjucks', function () {
     //Get .html and .njk files in pages folder
     return gulp.src('public/stage/*.njk')
     // Render template with nunjucksRender
@@ -12,6 +13,12 @@ gulp.task('default', function () {
     .pipe(gulp.dest('public'))
 });
 
+gulp.task('css', function() {
+    return gulp.src('./public/bootstrap-4.4.1-dist/css/bootstrap.min.css')
+    .pipe(purify(['.public/**/*.js', './public/*.html']))
+    .pipe(gulp.dest('./public/css'))
+})
+
 //Minify HTML
 /* gulp.task('minify', () => {
   return gulp.src('src/*.html')
@@ -19,4 +26,4 @@ gulp.task('default', function () {
     .pipe(gulp.dest('./'));
 }); */
 
-//gulp.task('default', gulp.parallel('img', 'svg', 'nunjucksRender'));
+gulp.task('default', gulp.parallel('nunjucks', 'css'));
