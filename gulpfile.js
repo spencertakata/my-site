995930f2a9e8a8bf11dd5d0147a75af5ef886fe3
spencer <spencer.takata@gmail.com>
2020-03-28 18:16:05 -0700

    updated gulpfile syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 const gulp = require('gulp');
 const nunjucksRender = require('gulp-nunjucks-render');
-const purify = require('gulp-purifycss');
 
 
-gulp.task('nunjucks', function () {
+function nunjucks() {
     //Get .html and .njk files in pages folder
     return gulp.src('public/stage/*.njk')
     // Render template with nunjucksRender
@@ -12,12 +11,7 @@ gulp.task('nunjucks', function () {
         }))
     // output files in public folder
     .pipe(gulp.dest('public'))
-});
+};
 
-gulp.task('css', function() {
-    return gulp.src('./public/bootstrap-4.4.1-dist/css/bootstrap.css')
-    .pipe(purify(['.public/**/*.js', './public/*.html']))
-    .pipe(gulp.dest('./public/css'))
-})
 
-gulp.task('default', gulp.parallel('nunjucks', 'css'));
+exports.default = nunjucks
